fc73ffaffc7db43ba780119acbe1e1b18b0c7964
s111s <s111s.7es@gmail.com>
2025-02-19 11:18:41 +0700

    calling habithub function on movement network

--- movement_utils.ts
+++ movement_utils.ts
@@ -1,17 +1,22 @@
 import { Ed25519PrivateKey, Aptos, AptosConfig, Network, NetworkToNetworkName } from "@aptos-labs/ts-sdk";
 import dotenv from "dotenv";
 
-const APTOS_NETWORK: Network = NetworkToNetworkName[Network.DEVNET];
-const config = new AptosConfig({network: APTOS_NETWORK});
-const aptos = new Aptos(config);
+// const MOVEMENT_NETWORK: Network = NetworkToNetworkName[Network.CUSTOM];
+const config = new AptosConfig({
+    network: Network.CUSTOM,
+    fullnode: 'https://aptos.testnet.porto.movementlabs.xyz/v1',
+    faucet: 'https://fund.testnet.porto.movementlabs.xyz/',
+});
+
+const movement = new Aptos(config);
 
 dotenv.config();
 const OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY;
 
 const getSigner = async () => {
     const privateKey = new Ed25519PrivateKey(OWNER_PRIVATE_KEY);
-    const signer = await aptos.deriveAccountFromPrivateKey({privateKey});
+    const signer = await movement.deriveAccountFromPrivateKey({privateKey});
     return signer
 }
 
-export {getSigner, aptos}
+export {getSigner, movement}
--- movement_view.ts
+++ movement_view.ts
@@ -1,8 +1,8 @@
-import { aptos } from "./movement_utils";
+import { movement } from "./movement_utils";
 import { InputViewFunctionData } from "@aptos-labs/ts-sdk";
 
-const ACCOUNT = "b9cbcb36e290f1be7e1e151321d9d4679c3bea1392108ec85f9497c69200f5c1";
-const MODULE = "Campaign";
+const ACCOUNT = "0x1ca6e301cd10d0245a1952a6d376654f3cbb208ddef826be65ffe9503061b0cb";
+const MODULE = "CampaignDemo";
 
 const viewModuleFunction = async () => {
     const payload: InputViewFunctionData = {
@@ -10,7 +10,7 @@ const viewModuleFunction = async () => {
         typeArguments: [],
         functionArguments: []
     };
-    const output = (await aptos.view({payload}));
+    const output = (await movement.view({payload}));
     console.log(output);
 }
 
